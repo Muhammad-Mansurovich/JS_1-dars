77c33644e6ceaa753782ea52492017dc7e5b9a5e
Muhammad Murtazokulov <133627275+Muhammad-Mansurovich@users.noreply.github.com>
2023-05-23 00:58:16 +0500

    Bu JS 1-dars kodlari
    yuklangan kodlar

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*document.write("Men bodyga yozildim!");*/
+/*alert("Saytimizga xush kelibsiz!");*/
+/*let x = 5;
+let y = 10;
+x = 20;
+y = 25;*/
+/*let x = 20, y = 25, z = 30;*/
+/*let s = "Assalomu alaykum";*/
+/*document.write(salom);*/
+/* Data types in JS */
+/*let raqam = 35;             => number*/
+/*let matn = "JavaScript";    => string*/
+/* Arifmetika in JS */
+let x = 10;
+/* x + = 5; */ /* x = 10 + 5 */
+/* x - = 5; */ /* x = 10 - 5 */
+/* x * = 5; */ /* x = 10 * 5 */
+/* x / = 5; */ /* x = 10 / 5 */
+/* let qoldiq = x % 20; */
+/* let natija = x ** 5 ; */ /* 99*99*99*99*99 */
+/* ++x; */
+/* --x; */
+/* x++; */
+/* x--; */
+
+document.write(x-50);
+
+
+
+
+
+
